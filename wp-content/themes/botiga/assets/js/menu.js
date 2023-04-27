b15b697a6d4afffd6402e0e052401eb99b6113aa
freeman <iviewn@gmail.com>
2023-04-27 09:48:13 +0300

    menu

--- wp-content/themes/botiga/assets/js/menu.js
+++ wp-content/themes/botiga/assets/js/menu.js
@@ -1,3 +1,48 @@
 jQuery(document).ready(function() {
-    console.log( "ready!" );
+    
+    jQuery(".botiga-offcanvas-menu .botiga-dropdown .menu-item-has-children a, .dropdown-symbol").click(function(e){
+        e.preventDefault();
+        //jQuery(".sub-menu").toggleClass('toggled');
+        //alert("The paragraph was clicked.");
+    });
+    jQuery(".menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-has-children").click(function(e){
+        jQuery(".sub-menu").toggleClass('toggled');
+        jQuery(".botiga-offcanvas-menu").toggleClass('sub');
+    }); 
+    jQuery("#primary-menu .dropdown-symbol").click(function(e){
+        //alert("The paragraph was clicked.");
+        //jQuery(".botiga-offcanvas-menu").toggleClass('sub');
+        
+    }); 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
