b486fa31865ef9bf5f83f7e848036e27d1751c10
freeman <iviewn@gmail.com>
2023-04-27 10:06:52 +0300

    menu

--- wp-content/themes/botiga/assets/js/menu.js
+++ wp-content/themes/botiga/assets/js/menu.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function() {
     
-    jQuery(".botiga-offcanvas-menu .botiga-dropdown .menu-item-has-children a, .dropdown-symbol").click(function(e){
+    jQuery(".botiga-offcanvas-menu .botiga-dropdown .menu-item-has-children > a").click(function(e){
         e.preventDefault();
         //jQuery(".sub-menu").toggleClass('toggled');
         //alert("The paragraph was clicked.");
     });
-    jQuery(".menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-has-children").click(function(e){
+    jQuery(".menu-item.menu-item-type-post_type.menu-item-object-page.menu-item-has-children > a").click(function(e){
         jQuery(".sub-menu").toggleClass('toggled');
         jQuery(".botiga-offcanvas-menu").toggleClass('sub');
     }); 
