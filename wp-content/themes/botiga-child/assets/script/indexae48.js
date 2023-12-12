6608fadd20602f5ffddf895bbbe76502fed69f27
freeman <iviewn@gmail.com>
2023-12-12 20:16:28 +0300

    main slider

--- wp-content/themes/botiga-child/assets/script/indexae48.js
+++ wp-content/themes/botiga-child/assets/script/indexae48.js
@@ -1,73 +1,109 @@
+// main page main slider
+jQuery(document).ready(function () {
+    jQuery('.intro').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: true,
+        dots: false,
+        adaptiveHeight: false,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 766,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 390,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+        ]
+    });
+});
 // slick image galery set up
-jQuery(document).ready(function(){
-  jQuery('.bo-order-cake__slider-galery').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    adaptiveHeight: true,
-    // autoplay: true,
-    // autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 766,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 390,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: true,
-        }
-      },
-    ]
-  });
+jQuery(document).ready(function () {
+    jQuery('.bo-order-cake__slider-galery').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 766,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 390,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+        ]
+    });
 });
 
 // slick testimonials set up
-jQuery(document).ready(function(){
-  jQuery('.bo-testimonials__slider-galery').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    adaptiveHeight: true,
-    // autoplay: true,
-    // autoplaySpeed: 4000,
-    responsive: [
-      {
-        breakpoint: 766,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 390,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-          dots: false,
-        }
-      },
-    ]
-  });
+jQuery(document).ready(function () {
+    jQuery('.bo-testimonials__slider-galery').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 766,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 390,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    dots: false,
+                }
+            },
+        ]
+    });
 });
 
 //yandex.maps for header
